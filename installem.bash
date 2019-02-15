a9e1da17d836eebeb0755b54020b221963d81461
Ron Railsmith <railsmith4@outlook.com>
2019-02-15 15:30:19 -0800

    more

--- installem.bash
+++ installem.bash
@@ -6,6 +6,7 @@
 
 apt update
 apt -y upgrade
+apt -y install git
 apt -y install docker.io
 apt -y install ifupdown aufs-tools debootstrap docker-doc rinse zfs-fuse
 echo On AWS, and/or Oracle Cloud, do this:
