5b1e026a906a2fc21ea53de99d59be4dbe787d03
Ron Railsmith <railsmith4@outlook.com>
2019-02-15 15:48:38 -0800

    more

--- apt.bash
+++ apt.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt update
 apt -y upgrade
-apt -y install emacs ruby ruby-dev
+apt -y install emacs ruby ruby-dev wget curl
 apt -y install git apt autoconf bison build-essential
 apt -y install libssl-dev libyaml-dev libreadline6-dev
 apt -y install zlib1g-dev libncurses5-dev libffi-dev
