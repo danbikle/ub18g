4129a629affa5bd81f5bc77d29f92f4f3502d38d
Ron Railsmith <railsmith4@outlook.com>
2019-02-15 16:02:36 -0800

    more

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM ubuntu:18.04
 ADD apt.bash    /root/apt.bash
 ADD ann.bash    /root/ann.bash
 ADD ann/.gemrc  /root/.gemrc
+ADD ann/.emacs  /root/.emacs
 ADD ann/.bashrc /root/ann_bashrc
 RUN bash        /root/apt.bash
 RUN bash        /root/ann.bash
--- ann.bash
+++ ann.bash
@@ -2,8 +2,10 @@
 useradd -m -s /bin/bash -G sudo ann
 cp /root/ann_bashrc ~ann/.bashrc
 cp /root/.gemrc     ~ann/.gemrc
+cp /root/.emacs     ~ann/.emacs
 chown ann           ~ann/.bashrc
 chown ann           ~ann/.gemrc
+chown ann           ~ann/.emacs
 su - ann -c 'cd ~ann; git clone https://github.com/rbenv/rbenv      .rbenv'
 su - ann -c 'cd ~ann; git clone https://github.com/rbenv/ruby-build .rbenv/plugins/ruby-build'
 su - ann -c 'cd ~ann; rbenv install 2.6.1'
