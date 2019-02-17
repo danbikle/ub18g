36506c9de5920ed4905f6fde16d23dc369b48fe5
Dan Bikle <danbikle@outlook.com>
2019-02-16 22:25:04 -0800

    more

--- apt.bash
+++ apt.bash
@@ -1,6 +1,10 @@
 #!/bin/bash
 apt update
 apt -y upgrade
+
+export TZ=America/Los_Angeles
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
 apt -y install emacs ruby ruby-dev wget curl emacs
 apt -y install git apt autoconf bison build-essential
 apt -y install libssl-dev libyaml-dev libreadline6-dev
