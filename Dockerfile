9c373f4f622b906c45080f57e65cf0546689ce21
Dan Bikle <danbikle@outlook.com>
2019-02-17 14:02:26 -0800

    more

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,7 @@
 FROM ubuntu:18.04
 ADD apt.bash    /root/apt.bash
 ADD ann.bash    /root/ann.bash
-ADD ann/.gemrc  /root/.gemrc
-ADD ann/.emacs  /root/.emacs
-ADD ann/.bashrc /root/ann_bashrc
+ADD ann         /root/ann
 RUN bash        /root/apt.bash
 RUN bash        /root/ann.bash
 EXPOSE          23000 22
--- ann.bash
+++ ann.bash
@@ -1,14 +1,10 @@
 #!/bin/bash
 useradd -m -s /bin/bash -G sudo ann
-cp /root/ann_bashrc ~ann/.bashrc
-cp /root/.gemrc     ~ann/.gemrc
-cp /root/.emacs     ~ann/.emacs
-chown ann           ~ann/.bashrc
-chown ann           ~ann/.gemrc
-chown ann           ~ann/.emacs
+rsync -a /root/ann/ ~ann/
+chown -R ann        ~ann
 su - ann -c 'cd ~ann; git clone https://github.com/rbenv/rbenv      .rbenv'
 su - ann -c 'cd ~ann; git clone https://github.com/rbenv/ruby-build .rbenv/plugins/ruby-build'
 su - ann -c 'cd ~ann; rbenv install 2.6.1'
 su - ann -c 'cd ~ann; rbenv global  2.6.1'
 su - ann -c 'cd ~ann; gem install rails -v 5.2.2'
-su - ann -c 'cd ~ann; rails new ann100'
+su - ann -c 'cd ~ann; rails new ann100 -JSCOM'
