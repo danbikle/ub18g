612d2ef6d96dcd62150c13399b4a74ddd5c38b1d
Dan Bikle <danbikle@outlook.com>
2019-02-17 12:57:01 -0800

    more

--- apt.bash
+++ apt.bash
@@ -10,4 +10,4 @@ apt -y install git apt autoconf bison build-essential
 apt -y install libssl-dev libyaml-dev libreadline6-dev
 apt -y install zlib1g-dev libncurses5-dev libffi-dev
 apt -y install libgdbm5 libgdbm-dev libsqlite3-dev
-apt -y install nodejs locales tzdata
+apt -y install nodejs locales tzdata openssh-server
