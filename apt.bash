cbbcf7e0ca20db688e5fb087717e54c57f86022e
Dan Bikle <danbikle@outlook.com>
2019-02-16 21:25:14 -0800

    more

--- apt.bash
+++ apt.bash
@@ -6,4 +6,4 @@ apt -y install git apt autoconf bison build-essential
 apt -y install libssl-dev libyaml-dev libreadline6-dev
 apt -y install zlib1g-dev libncurses5-dev libffi-dev
 apt -y install libgdbm5 libgdbm-dev libsqlite3-dev
-
+apt -y install nodejs locales
