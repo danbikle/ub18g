0ccad79416dc9589258e3ae3421a21ac9ee5ba84
Dan Bikle <danbikle@outlook.com>
2019-02-17 20:41:40 -0800

    more

--- ann.bash
+++ ann.bash
@@ -4,14 +4,12 @@ rsync -a /root/ann/ ~ann/
 chown -R ann        ~ann
 chgrp -R ann        ~ann
 
-echo root should do this later:
-echo /usr/sbin/sshd -D
-
-exit
-
 su - ann -c 'cd ~ann; git clone https://github.com/rbenv/rbenv      .rbenv'
 su - ann -c 'cd ~ann; git clone https://github.com/rbenv/ruby-build .rbenv/plugins/ruby-build'
 su - ann -c 'cd ~ann; rbenv install 2.6.1'
 su - ann -c 'cd ~ann; rbenv global  2.6.1'
 su - ann -c 'cd ~ann; gem install rails -v 5.2.2'
 su - ann -c 'cd ~ann; rails new ann100 -JSCOM'
+
+echo root should do this later:
+echo /usr/sbin/sshd -D
