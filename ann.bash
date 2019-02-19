a195d19d86ebf62fa80a1c5f6fe4587f4268f599
Dan Bikle <danbikle@outlook.com>
2019-02-18 18:39:43 -0800

    more

--- ann.bash
+++ ann.bash
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# ann.bash
+
+# This script should useradd ann and then do some setup chores.
+
 useradd -m -s /bin/bash -G sudo ann
 rsync -a /root/ann/ ~ann/
 chown -R ann        ~ann
